8701b59cc0d9cbd44780cd43e81243cb077a1fc5
Luke Siemens <lukesiemens@gmail.com>
2017-10-16 20:14:10 +0000

    fixed login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     
-    if user = authenticate_with_credentials(params[:email], params[:password])
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
       
       redirect_to '/'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,13 @@ class UsersController < ApplicationController
     end
 
     def create
-      @user = User.new(user_params)
-         if @user.save
-      session[:user_id] = @user.id
+
+      # user = User.find_by_email(params[:email])
+
+
+      user = User.new(user_params)
+         if user.save
+      session[:user_id] = user.id
          redirect_to '/'
     else
       redirect_to '/signup' #or render :new ?
@@ -17,6 +21,6 @@ class UsersController < ApplicationController
 private
 
  def user_params
-   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   params.require(:user).permit(:first_name,:last_name, :email, :password, :password_confirmation)
  end
 end
