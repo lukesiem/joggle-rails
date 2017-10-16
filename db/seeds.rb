e4fae21c020830ce4f52350484e9df67ddff9a96
Luke Siemens <lukesiemens@gmail.com>
2017-10-16 22:29:15 +0000

    fixed image problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@ Product.destroy_all
 cat1.products.create!({
   name:  'Shorts',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("Shorts_ftrhle.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/Shorts_oanucl.jpg",
   quantity: 10,
   price: 64.99
 })
@@ -47,7 +47,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Summer Shirt',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("SummerShirt_t960mu.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/SummerShirt_ydncke.jpg",
   quantity: 18,
   price: 124.99
 })
@@ -55,7 +55,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Frozen Pod',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("pod_ksjeih.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/pod_oocqjy.jpg",
   quantity: 4,
   price: 34.49
 })
@@ -63,7 +63,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Charles in Chard',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("charles_vutztn.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/charles_w5cxvx.jpg",
   quantity: 8,
   price: 25.00
 })
@@ -71,7 +71,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Employment',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("larger_g1osfu.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/larger_nbpjhb.jpg",
   quantity: 8,
   price: 1_225.00
 })
@@ -79,7 +79,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Under Water Wifi',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("wifi_qi67l8.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/wifi_ljgpmi.jpg",
   quantity: 82,
   price: 224.50
 })
@@ -88,7 +88,7 @@ cat1.products.create!({
 cat2.products.create!({
   name:  'Birthday Mix',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("30_wsdsib.png"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/30_jkttvd.png",
   quantity: 40,
   price: 164.49
 })
@@ -96,7 +96,7 @@ cat2.products.create!({
 cat2.products.create!({
   name:  'Troemel Taco',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("taco_jpbsdo.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/taco_agehx1.jpg",
   quantity: 3,
   price: 26.00
 })
@@ -104,7 +104,7 @@ cat2.products.create!({
 cat2.products.create!({
   name:  'Taj Box',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("photo_qo8fpy.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/photo_kovwxm.jpg",
   quantity: 32,
   price: 2_026.29
 })
@@ -112,7 +112,7 @@ cat2.products.create!({
 cat3.products.create!({
   name:  'Art Historical Canon',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("canon_rxwoqd.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/canon_imctrf.jpg",
   quantity: 320,
   price: 3_052.00
 })
@@ -120,14 +120,14 @@ cat3.products.create!({
 cat3.products.create!({
   name:  'Branding the Individual/Ubiquitous Authorship, Jack Fisher',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("brand_xnoeqc.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/brand_qwl81u.jpg",
   quantity: 2,
   price: 987.65
 })
 cat3.products.create!({
   name:  'Modigliani On Couch, Arron Graham',
   description: Faker::Hipster.paragraph(4),
-  image: cl_image_tag("couch_brp7av.jpg"),
+  image: "http://res.cloudinary.com/hyrcxwhtw/image/upload/v1508191799/stuff/couch_gqfkg4.jpg",
   quantity: 0,
   price: 2_483.75
 })
