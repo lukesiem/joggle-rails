c4c4b8c918cbc7c2a794dc44a94d11b6aa10b428
Luke Siemens <lukesiemens@gmail.com>
2017-10-17 11:53:54 +0000

    fixed cart page glitches:

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe User, type: :model do
         @user = User.new(first_name: nil,last_name: "Hero", email: "squarejaw@heromail.com",
           password: "save4life", password_confirmation: "save4life")
        expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Make Sure you have a first name")
    end
 
 
@@ -22,12 +23,14 @@ RSpec.describe User, type: :model do
         @user = User.new(first_name: 'Your',last_name: nil, email: "squarejaw@heromail.com",
           password: "save4life", password_confirmation: "save4life")
       expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("put in your last name")
     end
 
      it 'is not valid When email is not included' do
        @user = User.new(first_name: 'Your', last_name: 'Hero',email: nil,
           password: "save4life", password_confirmation: "save4life")
       expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("You need an email")
     end
 
       it 'is not valid When password is not included' do
@@ -46,12 +49,14 @@ RSpec.describe User, type: :model do
       @user = User.new(first_name: 'Your', last_name: 'Hero', email: "squarejaw@heromail.com",
         password: "save", password_confirmation:"save" )
       expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
   end
 
    it 'is not valid When password confirmation and password are not equal' do
       @user = User.new(first_name: 'Your', last_name: 'Hero', email: "squarejaw@heromail.com",
         password: "save4life", password_confirmation: 'savelife' )
       expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
   end
 
    it 'is not valid email is not unique' do
@@ -62,6 +67,7 @@ RSpec.describe User, type: :model do
         password: '1234567', password_confirmation: '1234567')
       
       expect(@user2).to_not be_valid
+      expect(@user2.errors.full_messages).to include("Email has already been used")
   
   end
 
